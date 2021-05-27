5c7676f68df2c07c7ca356befa02fcb0c1011d34
Kang Yong Wook <rkdfurdyddnr@naver.com>
2021-05-27 20:13:45 +0900

    add: class2 체스판칠하기

---
+++ solved.ac/src/class2/bj_2839_설탕배달.java
@@ -0,0 +1,18 @@
+package class2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj_2839_º≥≈¡πË¥ﬁ {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int result = 0;
+		
+		
+	}
+}
