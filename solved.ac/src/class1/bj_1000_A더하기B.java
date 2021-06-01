8c2ac10cb4a3f0ea673f85e77baf70ca84cfa4d6
Kang Yong Wook <rkdfurdyddnr@naver.com>
2021-06-01 17:59:44 +0900

    add:class1 A+B

---
+++ solved.ac/src/class1/bj_1000_A더하기B.java
@@ -0,0 +1,13 @@
+package class1;
+
+import java.util.Scanner;
+
+public class bj_1000_A¥ı«œ±‚B {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		System.out.println(M+N);
+	}
+}
