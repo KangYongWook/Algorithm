969664295281af0dc8bda00189b5024f648249b5
Kang Yong Wook <rkdfurdyddnr@naver.com>
2021-06-03 18:28:44 +0900

    feat: 카드 문제푸는중

---
+++ solved.ac/src/class2/bj_2164_카드2.java
@@ -0,0 +1,29 @@
+package class2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class bj_2164_Ä«µå2 {
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int result = 0;
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		for (int i = 1; i <= N; i++) {
+			list.add(i);
+		}
+		
+		while() {
+			
+		}
+		
+		
+		System.out.println(result);
+	}
+}
