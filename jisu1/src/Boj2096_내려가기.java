0175e3c5193b9bf419dde3b92d8cb76198323fbe
Jisu-Shin <sjszzang3053@gmail.com>
2025-01-22 22:41:05 +0900

    add : 17주차 과제

---
+++ jisu1/src/Boj2096_내려가기.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj2096_내려가기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] downGame = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            downGame[i][0] = Integer.parseInt(st.nextToken());
+            downGame[i][1] = Integer.parseInt(st.nextToken());
+            downGame[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dpMin = new int[n][3];
+        int[][] dpMax = new int[n][3];
+        dpMin[0] = downGame[0].clone();
+        dpMax[0] = downGame[0].clone();
+        for (int i = 1; i < n ; i++) {
+            dpMin[i][0] = downGame[i][0] + Math.min(dpMin[i-1][0],dpMin[i-1][1]);
+            dpMin[i][1] = downGame[i][1] + Math.min(Math.min(dpMin[i-1][0],dpMin[i-1][1]),dpMin[i-1][2]);
+            dpMin[i][2] = downGame[i][2] + Math.min(dpMin[i-1][1],dpMin[i-1][2]);
+
+            dpMax[i][0] = downGame[i][0] + Math.max(dpMax[i-1][0],dpMax[i-1][1]);
+            dpMax[i][1] = downGame[i][1] + Math.max(Math.max(dpMax[i-1][0],dpMax[i-1][1]),dpMax[i-1][2]);
+            dpMax[i][2] = downGame[i][2] + Math.max(dpMax[i-1][1],dpMax[i-1][2]);
+        }
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i=0;i<3;i++){
+            min = Math.min(min, dpMin[n-1][i]);
+            max = Math.max(max, dpMax[n-1][i]);
+        }
+
+        System.out.println(max+" "+min);
+
+    }
+}
+
+/*
+3
+1 0 0
+0 1 0
+0 0 1
+답 : 3 0
+ */
---
+++ jisu1/src/Boj9251_LCS.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj9251_LCS {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String colString = br.readLine(); // 기준 (열)
+        String rowString = br.readLine(); // (행)
+
+        int colSize = colString.length();
+        int rowSize = rowString.length();
+
+        int[][] dp = new int[rowSize+1][colSize+1];
+        for (int row = 1; row <= rowString.length(); row++) {
+            boolean sameFlag = false;
+            for (int col = 1; col <= colString.length(); col++) {
+
+                // 문자비교할때만 인덱스-1을 해줘야한다
+                // dp를 위해 Index 1 부터 값을 넣어줘야하기 때문 (index 0 에 값이 비어져있어야한다)
+                if (rowString.charAt(row-1) == colString.charAt(col-1)) {
+                    // 1%에서 틀림 dp[row][col-1] 로 했기 때문
+                    // 대각선에 값을 가져와야함
+                    dp[row][col] = dp[row-1][col-1]+1;
+                } else {
+                    dp[row][col] = Math.max(dp[row][col-1],dp[row-1][col]);
+                }
+            }
+        }
+
+        System.out.println(dp[rowSize][colSize]);
+    }
+}
+
+/*
+MWMHWCM
+CMPOFVF
+결과 : 3
+정답 : 2
+ */
