bed5b22818fc9267fd1e3904c746e5506bd28b7a
YongWookKang <rkdfurdyddnr@naver.com>
2025-05-22 02:53:42 +0900

    이직준비로 인한 미진한 스터디 쫒아가기 1주차

--- yongWook/solved.ac/src/class4/bj11725_트리의부모찾기.java
+++ yongWook/solved.ac/src/class4/bj11725_트리의부모찾기.java
@@ -1,4 +1,4 @@
-package class3_1;
+package class4;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,8 +6,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class bj11724_연결요소의개수 {
+public class bj11725_트리의부모찾기 {
 	
+	static int result[];
 	static boolean visited[];
 	static ArrayList<Integer> list[];
 	public static void main(String[] args) throws IOException {
@@ -15,18 +16,16 @@ public class bj11724_연결요소의개수 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken()); //정점의 개수
-		int M = Integer.parseInt(st.nextToken()); //간선의 개수
+		int N = Integer.parseInt(st.nextToken()); 
 		
 		list = new ArrayList[N+1];
+		result = new int[N+1];
 		visited = new boolean[N+1];
-		
 		for (int i = 0; i < N + 1; i++) {
 			list[i] = new ArrayList<>();
 	    }
 		 
-		 
-		for (int i = 0; i < M; i++) {
+		for (int i = 0; i < N-1; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			int from = Integer.parseInt(st.nextToken());
@@ -35,26 +34,25 @@ public class bj11724_연결요소의개수 {
 			list[to].add(from);
 		}
 		
-		int result = 0;
 		//정점이 1부터시작
-		for (int i = 1; i <= N; i++) {
-		     if (!visited[i]) {
-		         _dfs(i);
-		         result++;
-		     }
+		_dfs(1);
+		
+		for (int i = 2; i <= N; i++) {
+			System.out.println(result[i]);
 		}
 		
-		System.out.println(result);
 		
 	}
-	
 	
 	private static void _dfs(int start) {
 		visited[start] = true;
 		for (int to : list[start]) {
 		    if (!visited[to]) {
+		    	result[to] = start;
 		       _dfs(to);
 		    }
 		}
 	}
+	
+	
 }
