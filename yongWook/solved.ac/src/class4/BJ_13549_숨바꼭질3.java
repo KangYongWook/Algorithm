477f31856b9b17c82a7129ada40aaf75a5bedff8
YongWookKang <rkdfurdyddnr@naver.com>
2025-05-29 22:22:07 +0900

    알고리즘 스터디

---
+++ yongWook/solved.ac/src/class4/BJ_13549_숨바꼭질3.java
@@ -0,0 +1,91 @@
+package class4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BJ_13549_숨바꼭질3 {
+
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Point> q = new PriorityQueue<Point>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		boolean[] memo = new boolean[2000000]; //메모리 초과로인한 메모이제이션 추가
+		int[] depthMemo = new int[2000000]; //메모리 초과로인한 메모이제이션 추가
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		//동생이 작은 위치에 있으면 바로 끝내기
+		if(n  >= k ) {
+			System.out.println(n-k);
+			return;
+		}
+		
+		int result = 0;
+		//입력
+		q.add(new Point( n, 0));
+		while(!q.isEmpty()) {
+			
+			Point point = q.poll();
+			if( point.x == k ) {
+				result = point.depth;
+				break;
+			}
+			
+			//조건1
+			int tempX = point.x -1;
+			int depth = point.depth + 1  ;
+			if( tempX >= 0 && (!memo[tempX] || (memo[tempX] && depthMemo[tempX] > depth ))) { //도착지점보다 초과한값이 -1할때말고는 필요없어 해당 조건 부여.
+				memo[tempX] = true;
+				depthMemo[tempX] = depth;
+				q.add(new Point(tempX, depth));
+			}
+			
+			
+			//조건2
+			tempX = point.x + 1;
+			if( tempX <= 200000 && (!memo[tempX] || (memo[tempX] && depthMemo[tempX] > depth ))) {
+				memo[tempX] = true;
+				depthMemo[tempX] = depth;
+				q.add(new Point(tempX, depth));
+			}
+			
+			//조건3 순간이동
+			tempX = point.x * 2;
+			depth = point.depth;
+			if( tempX <= 200000 && (!memo[tempX] || (memo[tempX] && depthMemo[tempX] > depth )))  {
+				memo[tempX] = true;
+				depthMemo[tempX] = depth;
+				q.add(new Point( tempX, depth));
+			}
+		}
+		
+		System.out.println(result);
+		
+	}
+	
+	
+
+	static class Point implements Comparable<Point>{
+		
+		int x;
+		int depth;
+		
+		Point(int x, int depth){
+			this.x = x;
+			this.depth = depth;
+		}
+
+		@Override
+		public int compareTo(Point o) {
+			return (this.depth > o.depth ? 1 : -1);
+		}
+		
+		
+	}
+}
